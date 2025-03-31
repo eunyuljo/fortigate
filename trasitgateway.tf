c69bce7966154f7b63d3bb7146e4cd4db6db0a56
EC2 Default User <ec2-user@ip-172-31-10-133.ap-northeast-2.compute.internal>
2025-03-31 10:07:11 +0000

    tgw

--- trasitgateway.tf
+++ trasitgateway.tf
@@ -1,21 +1,25 @@
-# Transit Gateway 생성
+# 1. TGW 생성 (기본 라우팅 테이블 비활성화)
 resource "aws_ec2_transit_gateway" "tgw" {
   description = "Test Transit Gateway"
+
+  default_route_table_association = "disable"
+  default_route_table_propagation = "disable"
+
   tags = {
     Name = "Test-TGW"
   }
 }
 
-# Transit Gateway Route Table 생성
+# 2. TGW Route Table 생성 (명시적 테이블)
 resource "aws_ec2_transit_gateway_route_table" "tgw_route_table" {
   transit_gateway_id = aws_ec2_transit_gateway.tgw.id
 
   tags = {
-    Name = "TGW-Route-Table"
+    Name = "TGW-Custom-Route-Table"
   }
 }
 
-# TGW와 기존 VPC 연결 (Transit Gateway Attachment)
+# 3. VPC Attachments
 resource "aws_ec2_transit_gateway_vpc_attachment" "vpc1_attachment" {
   subnet_ids         = module.vpc1.intra_subnets
   transit_gateway_id = aws_ec2_transit_gateway.tgw.id
@@ -26,7 +30,6 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "vpc1_attachment" {
   }
 }
 
-# TGW와 새 VPC 연결 (Transit Gateway Attachment)
 resource "aws_ec2_transit_gateway_vpc_attachment" "vpc2_attachment" {
   subnet_ids         = module.vpc2.intra_subnets
   transit_gateway_id = aws_ec2_transit_gateway.tgw.id
@@ -37,90 +40,31 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "vpc2_attachment" {
   }
 }
 
-# VPC1 Route Table Map
-locals {
-  vpc1_public_route_table_map = {
-    for idx, rtb_id in module.vpc1.public_route_table_ids : "public_${idx}" => rtb_id
-  }
-  vpc1_private_route_table_map = {
-    for idx, rtb_id in module.vpc1.private_route_table_ids : "private_${idx}" => rtb_id
-  }
-}
-
-# VPC2 Route Table Map
-locals {
-  vpc2_public_route_table_map = {
-    for idx, rtb_id in module.vpc2.public_route_table_ids : "public_${idx}" => rtb_id
-  }
-  vpc2_private_route_table_map = {
-    for idx, rtb_id in module.vpc2.private_route_table_ids : "private_${idx}" => rtb_id
-  }
-}
-
-
-# # VPC1 Public 서브넷의 라우팅 테이블에 vpc2 네트워크 추가
-# resource "aws_route" "vpc1_public_to_vpc2" {
-#   for_each             = local.vpc1_public_route_table_map
-#   route_table_id       = each.value
-#   destination_cidr_block = "10.1.0.0/16" # VPC2 CIDR
-#   transit_gateway_id   = aws_ec2_transit_gateway.tgw.id
-# }
-
-# # VPC1 Private 서브넷의 라우팅 테이블에 vpc2 네트워크 추가
-# resource "aws_route" "vpc1_private_to_vpc2" {
-#   for_each             = local.vpc1_private_route_table_map
-#   route_table_id       = each.value
-#   destination_cidr_block = "10.1.0.0/16" # VPC2 CIDR
-#   transit_gateway_id   = aws_ec2_transit_gateway.tgw.id
-# }
-
-# # VPC2 Public 서브넷의 라우팅 테이블에 vpc1 네트워크 추가
-# resource "aws_route" "vpc2_public_to_vpc1" {
-#   for_each             = local.vpc2_public_route_table_map
-#   route_table_id       = each.value
-#   destination_cidr_block = "10.0.0.0/16" # VPC1 CIDR
-#   transit_gateway_id   = aws_ec2_transit_gateway.tgw.id
-# }
-
-# # VPC2 Private 서브넷의 라우팅 테이블에 vpc1 네트워크 추가
-# resource "aws_route" "vpc2_private_to_vpc1" {
-#   for_each             = local.vpc2_private_route_table_map
-#   route_table_id       = each.value
-#   destination_cidr_block = "10.0.0.0/16" # VPC1 CIDR
-#   transit_gateway_id   = aws_ec2_transit_gateway.tgw.id
-# }
-
-
-
-# VPC2의 0.0.0.0/0 트래픽을 VPC1로 전달
-resource "aws_ec2_transit_gateway_route" "default_route_vpc2_to_vpc1" {
-  destination_cidr_block         = "0.0.0.0/0"
+# 4. Attachments를 명시적 Route Table에 연결
+resource "aws_ec2_transit_gateway_route_table_association" "vpc1_assoc" {
   transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw_route_table.id
   transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.vpc1_attachment.id
 }
 
-# # Transit Gateway Route Table을 VPC Attachments에 연결
-# resource "aws_ec2_transit_gateway_route_table_association" "vpc1_association" {
-#   transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw_route_table.id
-#   transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.vpc1_attachment.id
-  
-# }
-# resource "aws_ec2_transit_gateway_route_table_association" "vpc2_association" {
-#   transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw_route_table.id
-#   transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.vpc2_attachment.id
-# }
+resource "aws_ec2_transit_gateway_route_table_association" "vpc2_assoc" {
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw_route_table.id
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.vpc2_attachment.id
+}
 
-
-
-# VPC1에서 TGW로 경로 전파 활성화
+# 5. 라우팅 전파 (Route Propagation)
 resource "aws_ec2_transit_gateway_route_table_propagation" "vpc1_propagation" {
   transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw_route_table.id
   transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.vpc1_attachment.id
 }
 
-# VPC2에서 TGW로 경로 전파 활성화
 resource "aws_ec2_transit_gateway_route_table_propagation" "vpc2_propagation" {
   transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw_route_table.id
   transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.vpc2_attachment.id
 }
 
+# 6. 명시적 라우팅 - VPC2의 트래픽을 VPC1로
+resource "aws_ec2_transit_gateway_route" "route_0_0_0_0_to_vpc1" {
+  destination_cidr_block         = "0.0.0.0/0"
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw_route_table.id
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.vpc1_attachment.id
+}
